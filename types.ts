43b0264f78288255c385938a47073fab6c8248f5
1111meorf1111-blip <1111meorf1111@gmail.com>
2026-01-20 14:33:06 +0300

    Add files via upload

---
+++ types.ts
@@ -0,0 +1,14 @@
+
+export interface Service {
+  id: number;
+  title: string;
+  description: string;
+  icon: string;
+}
+
+export interface Trainer {
+  name: string;
+  role: string;
+  image: string;
+  description: string;
+}
